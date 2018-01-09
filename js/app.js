0f0aa32aecc1cae2eb9f1ce6f6509488615ff6b5
CoreyD97 <corey-dean06@hotmail.com>
2018-01-09 10:56:07 +0000

    Added camera options

--- js/app.js
+++ js/app.js
@@ -5,20 +5,25 @@ var height = 1;
 var dist = 1;
 var lat = 0, lon = 0;
 var particles, particleSystem, particleMaterial;
+var ar = (window.innerWidth/window.innerHeight);
+var scale = 20;
+var boxSize = [scale*1.5, scale*1.5, scale*ar];
 
 var options = {
-    count: 100000,
+    camFollow: 'Comet',
+    count: 200000,
     // count: 512,
     particleSpeedMult: 0.01,
     drag: 0.0165,
-    size: 0.03,
+    size: 0.02,
     color: [255,255,255],
 
     boxSize: 20,
 
-    gravity: 1.5,
+    gravity: 1.7,
     gravitySize: 25,
     blackHoleSpeedMult: 0.2,
+    cometMaxSpeed: 0.5
 };
 
 init();
@@ -36,22 +41,22 @@ function init(){
     scene.background = new THREE.Color(255,255,255);
     cam = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
-    cam.position.set(-options.boxSize, height, 0);
+    cam.position.set(-scale, height, 0);
     cam.lookAt(scene.position);
 
-    var axis = new THREE.AxisHelper(options.boxSize/2);
-    scene.add(axis);
+    var axis = new THREE.AxisHelper(scale/2);
+    // scene.add(axis);
 
-    var cubegeo = new THREE.CubeGeometry(options.boxSize, options.boxSize, options.boxSize);
+    var cubegeo = new THREE.CubeGeometry(boxSize[0], boxSize[1], boxSize[2]);
     var cubemat = new THREE.MeshBasicMaterial( { color: 0x555555, wireframe: true});
     var cube = new THREE.Mesh(cubegeo, cubemat);
-    scene.add(cube);
+    // scene.add(cube);
 
-    var blackholegeo = new THREE.SphereGeometry(options.boxSize / 20, 10, 10, 0, Math.PI*2, 0, Math.PI);
-    var blackholetexture = new THREE.TextureLoader().load('img/flare-inverted-blue-purple3.png');
-    var blackholemat = new THREE.MeshBasicMaterial({ color: 0x000000, transparent: true});
+    var blackholegeo = new THREE.SphereGeometry(scale / 200, 10, 10, 0, Math.PI*2, 0, Math.PI);
+    var blackholetexture = new THREE.TextureLoader().load('./img/phobos2k.jpg');
+    var blackholemat = new THREE.MeshBasicMaterial({ /*color: 0x000000,*/ map: blackholetexture});
     blackhole = new THREE.Mesh(blackholegeo, blackholemat);
-    blackhole.velocity = new THREE.Vector3(Math.random(), Math.random(), Math.random());
+    blackhole.velocity = new THREE.Vector3(Math.random()*options.cometMaxSpeed, Math.random()*options.cometMaxSpeed, Math.random()*options.cometMaxSpeed);
     scene.add(blackhole);
 
     var skyboxmap = ["right1.png","left2.png","top3.png","bottom4.png","front5.png","back6.png"];
@@ -63,11 +68,18 @@ function init(){
 
     stats = new Stats();
     stats.showPanel(0);
-    document.body.appendChild(stats.dom);
+    // document.body.appendChild(stats.dom);
 }
 
 function createGUI(){
     var gui = new dat.GUI();
+
+    var interactionFolder = gui.addFolder("Mouse Interaction");
+    var mousecam = interactionFolder.add(options, "camFollow", ['Fixed', 'Rotate', 'Comet']).name("Camera Type");
+    mousecam.onFinishChange(function(val){
+       setCamFollow(val);
+    });
+
     var particleFolder = gui.addFolder("Particles");
     var countcontroller = particleFolder.add(options, 'count', 100, 300000).name("Count");
     countcontroller.onFinishChange(function(value){
@@ -87,10 +99,10 @@ function createGUI(){
     particleFolder.add(options, 'particleSpeedMult', 0.001, 0.1).name("Speed Multiplier");
     particleFolder.add(options, 'drag', 0.0, 0.025).name("Drag");
 
-    var blackHoleFolder = gui.addFolder("Black Holes");
+    var blackHoleFolder = gui.addFolder("Comet");
 
-    blackHoleFolder.add(options, 'gravity', 0.1, 2.5).name("Strength");
-    blackHoleFolder.add(options, 'gravitySize', 1.0, 50.0).name("Pull Radius");
+    blackHoleFolder.add(options, 'gravity', 0.1, 2.5).name("Gravity Force");
+    blackHoleFolder.add(options, 'gravitySize', 1.0, 50.0).name("Gravity Radius");
     blackHoleFolder.add(options, 'blackHoleSpeedMult', 0.01, 0.5).name("Speed Multiplier");
 
 
@@ -98,6 +110,7 @@ function createGUI(){
     blackHoleFolder.open();
     // gui.add(options, 'color', "#ffffff");
     // gui.add(options, 'gravity', 0.2);
+    gui.close();
 }
 
 function createParticles(count){
@@ -108,9 +121,9 @@ function createParticles(count){
         render();
     }
     for(var p=0; p<count-particles.vertices.length; p++){
-        var x = Math.random() * options.boxSize - (options.boxSize/2);
-        var y = Math.random() * options.boxSize - (options.boxSize/2);
-        var z = Math.random() * options.boxSize - (options.boxSize/2);
+        var x = Math.random() * boxSize[0] - (boxSize[0]/2);
+        var y = Math.random() * boxSize[1] - (boxSize[1]/2);
+        var z = Math.random() * boxSize[2] - (boxSize[2]/2);
         var particle = new THREE.Vector3(x,y,z);
         particle.velocity = new THREE.Vector3(Math.random(), Math.random(), Math.random());
         particles.vertices.push(particle);
@@ -161,6 +174,9 @@ function doFrame(){
     }
 
     doMove(blackhole, options.blackHoleSpeedMult, false);
+    if(options.camFollow === 'Comet'){
+        camFollow(blackhole);
+    }
     doReflect(blackhole);
 
     blackhole.geometry.verticesNeedUpdate = true;
@@ -172,13 +188,13 @@ function doFrame(){
 
 function doGravity(particle, gravSource){
     var dist = particle.distanceTo(gravSource.position);
-    var acceleration = options.gravity * options.boxSize/2 * 0.066726 / Math.pow(dist,2);
+    var acceleration = options.gravity * boxSize[1]/2 * 0.066726 / Math.pow(dist,2);
     if(acceleration > 1/options.gravitySize){
         var vecToCentre = new THREE.Vector3().subVectors(particle, gravSource.position);
         particle.velocity = new THREE.Vector3().subVectors(particle.velocity, vecToCentre.multiplyScalar(acceleration));
     }
 
-    var velocityScale = particle.velocity.length() / options.boxSize;
+    var velocityScale = particle.velocity.length() / boxSize[1];
     if(velocityScale > 1){
         particle.velocity.multiplyScalar(1/velocityScale);
     }
@@ -186,41 +202,41 @@ function doGravity(particle, gravSource){
 
 function doReflect(particle) {
     if(particle.x && particle.y && particle.z){
-        if(particle.x > options.boxSize/2){
+        if(particle.x > boxSize[0]/2){
             particle.velocity.reflect(new THREE.Vector3(-1,0,0));
         }
-        if(particle.x < -options.boxSize/2){
+        if(particle.x < -boxSize[0]/2){
             particle.velocity.reflect(new THREE.Vector3(1,0,0));
         }
-        if(particle.y > options.boxSize/2){
+        if(particle.y > boxSize[1]/2){
             particle.velocity.reflect(new THREE.Vector3(0,-1,0));
         }
-        if(particle.y < -options.boxSize/2){
+        if(particle.y < -boxSize[1]/2){
             particle.velocity.reflect(new THREE.Vector3(0,1,0));
         }
-        if(particle.z > options.boxSize/2){
+        if(particle.z > boxSize[2]/2){
             particle.velocity.reflect(new THREE.Vector3(0,0,-1));
         }
-        if(particle.z < -options.boxSize/2){
+        if(particle.z < -boxSize[2]/2){
             particle.velocity.reflect(new THREE.Vector3(0,0,1));
         }
     }else{
-        if(particle.position.x > options.boxSize/2){
+        if(particle.position.x > boxSize[0]/2){
             particle.velocity.reflect(new THREE.Vector3(-1,0,0));
         }
-        if(particle.position.x < -options.boxSize/2){
+        if(particle.position.x < -boxSize[0]/2){
             particle.velocity.reflect(new THREE.Vector3(1,0,0));
         }
-        if(particle.position.y > options.boxSize/2){
+        if(particle.position.y > boxSize[1]/2){
             particle.velocity.reflect(new THREE.Vector3(0,-1,0));
         }
-        if(particle.position.y < -options.boxSize/2){
+        if(particle.position.y < -boxSize[1]/2){
             particle.velocity.reflect(new THREE.Vector3(0,1,0));
         }
-        if(particle.position.z > options.boxSize/2){
+        if(particle.position.z > boxSize[2]/2){
             particle.velocity.reflect(new THREE.Vector3(0,0,-1));
         }
-        if(particle.position.z < -options.boxSize/2){
+        if(particle.position.z < -boxSize[2]/2){
             particle.velocity.reflect(new THREE.Vector3(0,0,1));
         }
     }
@@ -256,10 +272,14 @@ function mouseScroll(event) {
     if(event.deltaY > 0 && dist <= 2-scrollstep){
         dist += scrollstep;
     }
+    if(options.camFollow === 'Rotate'){
+        rotate(lat,lon);
+    }
 }
 
 renderer.domElement.addEventListener("mousemove", mouseLook, false);
 function mouseLook(event){
+    if(options.camFollow !== 'Rotate') return;
     var width = renderer.getSize().width;
     var mousex = event.clientX - (width / 2);
     lat = mousex / width * 2;
@@ -270,12 +290,30 @@ function mouseLook(event){
     rotate(lat, lon);
 }
 
+function setCamFollow(val) {
+    if(val === 'Fixed') {
+        cam.position.x = -scale / 2;
+        cam.position.z = 0;
+        cam.position.y = 0;
+        cam.lookAt(scene.position);
+        cam.updateProjectionMatrix();
+    }
+}
+
 function rotate(lat, lon){
     // -1 <= val <= 1
-    cam.position.x = -options.boxSize * Math.cos(lat) * dist;
-    cam.position.z = options.boxSize * Math.sin(lat) * dist;
-    cam.position.y = -options.boxSize * Math.sin(lon) * dist;
+    // if(lat > 0.75) lat = 0.75;
+    // else if(lat < -0.75) lat = -0.75;
+    // if(lon > 0.1) lon = 0.1;
+    // if(lon < -0.1) lon = -0.1;
+    lat *= 0.75;
+    lon *= 0.5;
+
+    cam.position.x = -scale/2 * Math.cos(lat) * dist;
+    cam.position.z = boxSize[1] * Math.sin(lat) * dist;
+    cam.position.y = -scale/2 * Math.sin(lon) * dist;
     cam.lookAt(scene.position);
+    cam.updateProjectionMatrix();
 }
 
 window.addEventListener("resize", windowResize, false);
@@ -285,3 +323,12 @@ function windowResize(){
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
+function camFollow(object){
+    var pos = object.position;
+    var vel = object.velocity;
+
+    cam.position.x = (pos.x + (-vel.x * (dist*25))) * Math.cos(lat);
+    cam.position.y = (pos.y + (-vel.y * (dist*25))) * Math.sin(lat);
+    cam.position.z = (pos.z + (-vel.z * (dist*25))) * Math.sin(lon);
+    cam.lookAt(pos);
+}
